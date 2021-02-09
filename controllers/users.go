d1a9b895162b4aefc1c6401109a908ddac1d0cb7
pavanish <pavanish.kumar@gmail.com>
2021-02-09 14:38:02 +0800

    signup form completed: sec 7.4

---
+++ controllers/users.go
@@ -0,0 +1,42 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"../views"
+)
+
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+	UserName string `schema:"username"`
+}
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a temporay user Create.")
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, "Email is ", form.Email)
+	fmt.Fprintln(w, "password is ", form.Password)
+	fmt.Fprintln(w, "user name is ", form.UserName)
+
+}
