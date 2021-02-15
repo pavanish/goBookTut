874cf605e654c6acf6f2b3706db98aa5c6d039b9
pavanish <pavanish.kumar@gmail.com>
2021-02-15 14:57:14 +0800

    chapter 9 completed

---
+++ models/users.go
@@ -0,0 +1,99 @@
+package models
+
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	ErrorNotFound = errors.New("models: resource not found")
+	ErrInvalidID  = errors.New("models: ID provided was invalid")
+)
+
+type User struct {
+	gorm.Model
+	UserName string
+	Email    string `gorm:"not null;unique_index"`
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+}
+
+// Update will update the provided user with all of the data
+func (us *UserService) Update(user *User) error {
+	return us.db.Save(user).Error
+}
+
+// Delete will update the Delete the user with provided ID
+func (us *UserService) Delete(id uint) error {
+	if id == 0 {
+		return ErrInvalidID
+	}
+	user := User{Model: gorm.Model{ID: id}}
+	return us.db.Save(user).Error
+}
+
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	db := us.db.Where("id= ?", id)
+	err := first(db, &user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (us *UserService) ByEmail(email string) (*User, error) {
+	var user User
+	db := us.db.Where("email= ?", email)
+	err := first(db, &user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func first(db *gorm.DB, dst interface{}) error {
+	err := db.First(dst).Error
+	if err == gorm.ErrRecordNotFound {
+		return ErrorNotFound
+	}
+	return err
+}
+
+func (us *UserService) AutoMigrate() error {
+	if err := us.db.AutoMigrate(&User{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (us *UserService) DestructiveReset() error {
+	err := us.db.DropTableIfExists(&User{}).Error
+	if err != nil {
+		return err
+	}
+	return us.AutoMigrate()
+}
